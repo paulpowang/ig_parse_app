7040fadd0cbaa59aee54df8f4130e69bf6c7ea3d
paul <paul@sfs-wifi-dhcp-10-143-148-103.wifi.sfsu.edu>
2018-09-20 16:50:18 -0700

    first setup compelete

---
+++ Pods/Target Support Files/Pods-igParseApp/Pods-igParseApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_igParseApp : NSObject
+@end
+@implementation PodsDummy_Pods_igParseApp
+@end
---
+++ igParseApp/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  igParseApp
+//
+//  Created by paul on 9/20/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!){
+            (user: PFUser?, error: Error?) in
+            if user != nil{
+                print("You're Login")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success{
+                print("Yay, created a user!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+
+}
